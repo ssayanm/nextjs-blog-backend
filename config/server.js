49fb6c0c15d9f84dc02ea9ce4d55b373d27ff906
Sayan Mukherjee <hello@sayanmukherjee.com>
2021-06-09 18:38:48 +0530

    updating role permissions

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1345),
+  port: env.int("PORT", 1347),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
